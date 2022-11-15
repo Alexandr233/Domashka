3c342f7ee51e47c40038bb16222452fb5b6a9222
alexandr <sasasha1999@mail.ru>
2022-11-15 19:56:04 +0300

    zadanie2

---
+++ zadanie2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Какое число больше из двух чисел?");
+
+Console.WriteLine("Введите первое число и нажмите Enter ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе  число и нажмите Enter ");
+int numB = int.Parse(Console.ReadLine());
+
+if(numA > numB)
+{
+    Console.WriteLine($"max = {numA}");
+}
+else
+{
+    Console.WriteLine($"max = {numB}");
+}
