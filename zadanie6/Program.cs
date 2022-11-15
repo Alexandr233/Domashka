09bea0dda5735a2d71426f173f72ee9941f2f0a4
alexandr <sasasha1999@mail.ru>
2022-11-15 20:09:46 +0300

    zadanie6

---
+++ zadanie6/Program.cs
@@ -0,0 +1,13 @@
+﻿//Определения четности и нечетности числа введенного пользователем
+Console.WriteLine("Введите число!");
+
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if ((numA % 2) == 0)
+{
+    Console.WriteLine($"Число четное!");
+}
+else
+{
+    Console.WriteLine($"Число нечетное");
+}
