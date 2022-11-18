8d90b0263f7c266e31a1d33fd7947c574c08c4cd
alexandr <sasasha1999@mail.ru>
2022-11-18 21:48:52 +0300

    zadanie15

---
+++ zadanie15/Program.cs
@@ -0,0 +1,25 @@
+﻿double number;
+
+while(true)
+{
+    Console.Write("Введите число от 1 до 7! ");
+    if(double.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine("Не правельный ввод! Введите число от 1 до 7!");
+}
+if(number < 6)
+{
+    Console.WriteLine($"{number} -> нет!");
+}
+else
+{
+    Console.WriteLine("Не правельный ввод! Введите число от 1 до 7!");
+}
+if(number == 6)
+{
+    Console.WriteLine($"{number} -> да!");
+}
+else if(number == 7)
+{
+    Console.WriteLine($"{number} -> да!");
+}
