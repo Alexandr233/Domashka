32559918384309d49b0adafbd24df6f10d0c153d
alexandr <sasasha1999@mail.ru>
2022-11-18 22:02:38 +0300

    zadanie

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -1,8 +1,15 @@
 ﻿
 Console.WriteLine("Введите трехзначное число! ");
 int a = int.Parse(Console.ReadLine());
+if(a > 100 && a < 1000)
+{
+    Console.Write($"{a} -> ");
+}
+else 
+{
+    Console.WriteLine("Не правильный ввод! Ведите трехзначное число! ");
+}
 
 int b = a % 100 / 10;
 
-
-Console.WriteLine($"{a} -> {b}");
+Console.Write($"{b}");
