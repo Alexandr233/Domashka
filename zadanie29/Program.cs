2a853739b2306dc949de2a3242269e9509e8e1d0
alexandr <sasasha1999@mail.ru>
2022-11-27 20:58:27 +0300

    zadanie29

---
+++ zadanie29/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[8];
+
+FillAray(array);
+PrintArray(array);
+///Заполнение массива
+void FillAray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 100);
+}
+///Вывод массива
+void PrintArray (int[] array)
+{
+    Console.Write($"[ ");
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ",");
+    Console.Write($"]");
+}
