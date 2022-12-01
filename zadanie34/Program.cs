c3d1b4142bdddb9f7508cb23889cb37ea8ff7812
alexandr <sasasha1999@mail.ru>
2022-12-01 23:30:00 +0300

    zadanie34

---
+++ zadanie34/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите размер массива! ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] number = new int[size];
+RandomNumbers(number);
+PrintArray(number);
+int count = 0;
+
+for (int i = 0; i < number.Length; i++)
+if (number[i] % 2 == 0)
+count++;
+
+Console.Write($" -> {count} колличество чётных");
+Console.WriteLine();
+
+void RandomNumbers(int[] number)
+{
+    for(int i = 0; i < number.Length; i++)
+    {
+        number[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] number)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < number.Length; i++)
+    {
+        Console.Write(number[i] + " ");
+    }
+    Console.Write("]");
+    
+ }
