4e5f72373d05d92c994903358d52e554e5d7fd4b
alexandr <sasasha1999@mail.ru>
2022-12-05 21:40:26 +0300

    zadanie41

---
+++ zadanie41/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+Console.WriteLine("Введите количество элементов: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int[] array = PrintNumbers(numbers);
+Console.WriteLine($"Чисел больше нуля -> {NumberElements(array)}");
+
+int[] PrintNumbers(int num)
+{
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите число {i} -> ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int NumberElements(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
